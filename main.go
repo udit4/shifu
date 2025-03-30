b0a4e82f9d77ae353c9db71659cd7726bd39925c
Udit <udit@Udits-MacBook-Pro.local>
2025-03-30 13:58:48 +0530

    added template.html and http service

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"html/template"
 	"log"
+	"net/http"
+	"os"
 )
 
-func main() {
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("template.html")
+	if err != nil {
+		log.Printf("Error loading template : %+v\n", err)
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+	tmpl.Execute(w, nil)
+}
 
-	log.Printf("hello udit")
+func main() {
+	if _, err := os.Stat("template.html"); os.IsNotExist(err) {
+		log.Fatal("template.html file not found")
+	}
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
+	log.Println("Starting server on :9000")
+	http.ListenAndServe(":9000", nil)
 }
